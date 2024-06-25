8a451ccbf9043b7236ae84f34be790534921b3f4
ABEL SAGA <68258483+sg-abel@users.noreply.github.com>
2024-06-25 09:32:31 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        var usuario = "Abel";
+        var titulo = "Ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+        
+        var num1 = 3;
+        var num2 = 4;
+
+        System.out.println(num1 + num2);//suma de numeros por impresion
+
+        var suma = num1 + num2;//suma de numeros por variable
+        System.out.println("suma = " + suma);
+    }
+}
